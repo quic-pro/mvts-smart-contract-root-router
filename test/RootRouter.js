a38f55b897ad54bc636c73aad08cee1dcb1df3e3
Sergey <50526865+Kosrotoff@users.noreply.github.com>
2022-12-08 23:40:05 +0700

    Added tests for smart contract management methods

--- test/RootRouter.js
+++ test/RootRouter.js
@@ -100,13 +100,9 @@ describe('RootRouter', async () => {
         await expect(contract.getCodeData(POOL_SIZE)).to.be.revertedWith('Invalid code!');
     });
 
-    it('should allow withdrawal of funds only to the owner', async () => {
-        await expect(contract.connect(accounts[0]).withdraw()).to.be.revertedWith('Ownable: caller is not the owner');
-        await expect(contract.connect(contractOwner).withdraw()).not.to.be.revertedWithoutReason()
-    });
-
     it('should withdraw all funds to the owner', async () => {
         await contract.connect(accounts[0]).mint(100, {value: BUY_PRICE.toString()});
+        await expect(contract.connect(accounts[0]).withdraw()).to.be.revertedWith('Ownable: caller is not the owner');
 
         const contractOwnerInitialBalance = await provider.getBalance(contractOwner.address);
         await contract.connect(contractOwner).withdraw();
@@ -117,6 +113,101 @@ describe('RootRouter', async () => {
 
         expect(contractBalance).to.be.equal(0);
         expect(+contractOwnerDeltaBalance).to.be.closeTo(+formatEther(BUY_PRICE.toString()), 1e-4);
+    });
+
+    it('should be set a new buy price', async () => {
+        const newBuyPrice = parseEther('5');
+
+        await expect(contract.connect(accounts[0]).setBuyPrice(newBuyPrice)).to.be.revertedWith('Ownable: caller is not the owner');
+        await contract.connect(contractOwner).setBuyPrice(newBuyPrice);
+
+        expect(await contract.buyPrice()).to.be.equal(newBuyPrice);
+    });
+
+    it('should be set a new subscription price', async () => {
+        const newSubscriptionPrice = parseEther('3');
+
+        await expect(contract.connect(accounts[0]).setSubscriptionPrice(newSubscriptionPrice)).to.be.revertedWith('Ownable: caller is not the owner');
+        await contract.connect(contractOwner).setSubscriptionPrice(newSubscriptionPrice);
+
+        expect(await contract.subscriptionPrice()).to.be.equal(newSubscriptionPrice);
+    });
+
+    it('should be set a new mode change price', async () => {
+        const newModeChangePrice = parseEther('3');
+
+        await expect(contract.connect(accounts[0]).setModeChangePrice(newModeChangePrice)).to.be.revertedWith('Ownable: caller is not the owner');
+        await contract.connect(contractOwner).setModeChangePrice(newModeChangePrice);
+
+        expect(await contract.modeChangePrice()).to.be.equal(newModeChangePrice);
+    });
+
+    it('should be set a new subscription duration', async () => {
+        const newSubscriptionDuration = parseEther('3');
+
+        await expect(contract.connect(accounts[0]).setSubscriptionDuration(newSubscriptionDuration)).to.be.revertedWith('Ownable: caller is not the owner');
+        await contract.connect(contractOwner).setSubscriptionDuration(newSubscriptionDuration);
+
+        expect(await contract.subscriptionDuration()).to.be.equal(newSubscriptionDuration);
+    });
+
+    it('should be set a new holding duration', async () => {
+        const newHoldingDuration = parseEther('3');
+
+        await expect(contract.connect(accounts[0]).setHoldingDuration(newHoldingDuration)).to.be.revertedWith('Ownable: caller is not the owner');
+        await contract.connect(contractOwner).setHoldingDuration(newHoldingDuration);
+
+        expect(await contract.holdingDuration()).to.be.equal(newHoldingDuration);
+    });
+
+    it('should be set a new holding duration', async () => {
+        const newTtl = 3 * DAY;
+
+        await expect(contract.connect(accounts[0]).setTtl(newTtl)).to.be.revertedWith('Ownable: caller is not the owner');
+        await contract.connect(contractOwner).setTtl(newTtl);
+
+        expect(await contract.ttl()).to.be.equal(newTtl);
+    });
+
+    it('should be set a new default sip domain', async () => {
+        const newDefaultSipDomain = 'sip.test';
+
+        await expect(contract.connect(accounts[0]).setDefaultSipDomain(newDefaultSipDomain)).to.be.revertedWith('Ownable: caller is not the owner');
+        await contract.connect(contractOwner).setDefaultSipDomain(newDefaultSipDomain);
+
+        expect(await contract.defaultSipDomain()).to.be.equal(newDefaultSipDomain);
+    });
+
+    it('should be set a new base uri', async () => {
+        const newBaseUri = 'https://test.test/';
+
+        await expect(contract.connect(accounts[0]).setBaseUri(newBaseUri)).to.be.revertedWith('Ownable: caller is not the owner');
+        await contract.connect(contractOwner).setBaseUri(newBaseUri);
+
+        expect(await contract.baseUri()).to.be.equal(newBaseUri);
+    });
+
+    it('should be set a new code blocked status', async () => {
+        const code = POOL_SIZE - 1;
+        const {isBlocked} = await contract.getCodeData(code);
+
+        await expect(contract.connect(accounts[0]).setCodeBlockedStatus(code, !isBlocked)).to.be.revertedWith('Ownable: caller is not the owner');
+        await expect(contract.connect(contractOwner).setCodeBlockedStatus(POOL_SIZE, !isBlocked)).to.be.revertedWith('Invalid code!');
+        await contract.connect(contractOwner).setCodeBlockedStatus(code, !isBlocked);
+
+        expect((await contract.getCodeData(code)).isBlocked).to.be.equal(!isBlocked);
+    });
+
+    it('should be set a new subscription end time', async () => {
+        const code = POOL_SIZE - 1;
+        await contract.mint(code);
+        const {subscriptionEndTime} = await contract.getCodeData(code);
+
+        await expect(contract.connect(accounts[0]).setCodeSubscriptionEndTime(code, subscriptionEndTime + DAY)).to.be.revertedWith('Ownable: caller is not the owner');
+        await expect(contract.connect(contractOwner).setCodeSubscriptionEndTime(POOL_SIZE, subscriptionEndTime + DAY)).to.be.revertedWith('Invalid code!');
+        await contract.connect(contractOwner).setCodeSubscriptionEndTime(code, subscriptionEndTime + DAY);
+
+        expect((await contract.getCodeData(code)).subscriptionEndTime).to.be.equal(subscriptionEndTime + DAY);
     });
 
     /*
